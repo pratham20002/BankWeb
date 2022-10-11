6d8d36b4a2c5dcc797425d83d1223e49ad196928
pratham20002 <satputepratham46@gmail.com>
2022-10-11 17:01:27 +0530

    allowed host changed

--- BankWeb/BankWeb/settings.py
+++ BankWeb/BankWeb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6hu6ksqzjez*t+ct34gunwmd=^x80hddgty%p9)8$8*$&66j8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bank-noob.herokuapp.com']
 
 
 # Application definition
