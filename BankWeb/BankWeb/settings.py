418cf612a8f29572b6bd62a07cf80031de0a7925
pratham20002 <satputepratham46@gmail.com>
2022-10-11 16:55:32 +0530

    static collection white noise

--- BankWeb/BankWeb/settings.py
+++ BankWeb/BankWeb/settings.py
@@ -131,7 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+#  add here always this 3 lines
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# ////////////////////////////////////////////////////////////////////////////////////
+
 
 # Added Manually...............!
 
